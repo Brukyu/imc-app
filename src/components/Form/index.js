aac4d16a40753db785515428bccf5a39c3d29970
brukyu <bbrunoigorr@gmail.com>
2023-03-04 09:10:34 -0300

    add age button

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,64 +1,82 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import ResultIMC from "./ResultIMC/";
 import styles from "./style";
 
-export default function Form(){
+export default function Form() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [messageIMC, setMessageIMC] = useState(null);
+  const [imc, setIMC] = useState(null);
+  const [textButton, setTextButton] = useState("Resultado");
+  const [selectedButton, setSelectedButton] = useState(null);
 
-const [height, setHeight]= useState(null)
-const [weight,setWeight]= useState(null)
-const [messageIMC, setMessageIMC]= useState("preencha o peso e altura")
-const [imc, setIMC]= useState(null)
-const [textButton, setTextButton]= useState("Calcular")
+  function imcCalculator() {
+    return setIMC((weight / (height * height)).toFixed(2));
+  }
 
-function imcCalculator (){
-    return setIMC ((weight/(height*height)).toFixed(2))
-}
-
-function validationIMC(){
-    if(weight != null && height != null){
-        imcCalculator()
-        setHeight(null)
-        setWeight(null)
-        setMessageIMC("Seu imc é igual:")
-        setTextButton("Calcular novamente")
-        return
+  function validationIMC() {
+    if (weight != null && height != null) {
+      imcCalculator();
+      setHeight(null);
+      setWeight(null);
+      setMessageIMC("Seu imc é igual:");
+      setTextButton("Calcular novamente");
+      return;
     }
-    setIMC(null)
-    setTextButton("Calcular")
-    setMessageIMC("preencha o texto e altura")
-}
+    setIMC(null);
+    setTextButton("Resultado");
+    setMessageIMC("Preencha o texto e altura");
+  }
 
-    return(
-        <View style={styles.FormContext}>
-          <View style={styles.form}>
+  return (
+    <View style={styles.FormContext}>
+      <View style={styles.ageButtonContainer}>
+        <TouchableOpacity
+          style={[
+            styles.ageButton,
+            selectedButton === "adulto" && { backgroundColor: "#2996f0" },
+          ]}
+          onPress={() => setSelectedButton("adulto")}
+        >
+          <Text style={[styles.ageButtonText, selectedButton === "adulto" && { color: "#FFFFFF" }]}>Adulto</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.ageButton,
+            selectedButton === "idoso" && { backgroundColor: "#2996f0" },
+          ]}
+          onPress={() => setSelectedButton("idoso")}
+        >
+          <Text style={[styles.ageButtonText, selectedButton === "idoso" && { color: "#FFFFFF" }]}>Idoso</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={[
+            styles.input,
+            { marginBottom: 30, paddingLeft: 30, fontSize: 14, fontWeight: "300" },
+          ]}
+          placeholder="Digite Sua Altura"
+          placeholderTextColor={"#000000"}
+          keyboardType="numeric"
+          value={height}
+          onChangeText={(text) => setHeight(text)}
+        />
+        <TextInput
+          style={[styles.input, { paddingLeft: 30, fontSize: 14, fontWeight: "300" }]}
+          placeholder="Digite Seu Peso"
+          placeholderTextColor={"#000000"}
+          keyboardType="numeric"
+          value={weight}
+          onChangeText={(text) => setWeight(text)}
+        />
 
-            <Text style={styles.formLable}>Altura</Text>
-            <TextInput style={styles.input}
-              placeholder="Digite sua altura"
-              keyboardType="numeric"
-              value={height}
-              onChangeText={(text) => setHeight(text)}
-            />
-
-            <Text style={styles.formLable}>Peso</Text>
-            <TextInput style={styles.input}
-              placeholder="Digite seu peso"
-              keyboardType="numeric"
-              value={weight}
-              onChangeText={(text) => setWeight(text)}
-            />
-
-            <TouchableOpacity style ={styles.ButtonCalculator}
-              onPress={()=> {
-                validationIMC()
-              }}
-            >
-              <Text style={styles.textButtonCalculator}>{textButton}</Text>
-            </TouchableOpacity>
-          </View> 
-
-          <ResultIMC messageResultIMC={messageIMC} ResultIMC={imc} />
-        </View>
-    )
+        <TouchableOpacity style={styles.ButtonCalculator} onPress={() => validationIMC()}>
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
+      </View>
+      <ResultIMC messageResultIMC={messageIMC} ResultIMC={imc} />
+    </View>
+  );
 }
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./style";
 
 export default function Title(){
     return(
-        <View>
-            <Text>Cantadas Enfadonhas</Text>
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>Calculadora IMC</Text>
+            <Text style={styles.textDescription}>O Índice de Massa Corporal (IMC) é uma medida que avalia a relação entre peso e altura de uma pessoa.</Text>
         </View>
     )
 }
