9aec9932fc3ca9cba220ec3025a437d7dfb7aabb
brukyu <bbrunoigorr@gmail.com>
2023-03-04 09:12:38 -0300

    updating the code

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ import Form from './src/components/Form/';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Title/>
-      <Form/>
-    </View>
-  );
+      <View style={styles.container}>
+        <Title/>
+        <Form/>
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#2996f0",
     flex: 1,
-    backgroundColor: '#e0e5e5',
-    paddingTop:80,
+    paddingTop: 200,
+    alignItems: 'center',
+    justifyaontent: 'center',
   },
-});
+  },);
